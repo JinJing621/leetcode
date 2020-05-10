58b2ac027b91285a16748e701fbba6783a0d3b0f
JinJing621 <55825590+JinJing621@users.noreply.github.com>
2020-05-10 14:11:45 +0800

    Q7

--- Q7.java
+++ Q7.java
@@ -1,16 +1,25 @@
 package com.daixia.leetcode;
 
 public class Q7 {
-    public int reverse(int x) {
-        int sign = 1;
-        if(x < 0){
-            sign = -1;
-        }
+    public static int reverse(int x) {
         int res = 0;
         while (Math.abs(x) > 0){
+            System.out.println(res);
             res = res*10 + x%10;
             x = x/10;
+            if(Math.abs(x) > 0 && (res > 214748364 || (res == 214748364 && x > 7))){
+                return 0;
+            }
+            if(Math.abs(x) > 0 && (res < -214748364 || (res == -214748364 && x < -8))){
+                return 0;
+            }
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        int a = -2147483412;
+        //System.out.println(-214384741 > -214748364);
+        System.out.println(reverse(a));
     }
 }

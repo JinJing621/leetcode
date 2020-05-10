ee00a648c1794fb01499976322f0adfd5d56858c
JinJing621 <55825590+JinJing621@users.noreply.github.com>
2020-05-10 15:01:26 +0800

    Add files via upload

--- Q5.java
+++ Q5.java
@@ -6,39 +6,34 @@ public class Q5 {
         if(length == 0){
             return "";
         }
-        int center = 0;
-        int maxSubStr = 1;
+        Integer left = 0;
+        Integer right = 0;
+        Integer maxRes = 1;
         for(int i=0; i<length; i++){
-            int j=1;
-            for(j=1; j<=Math.min(i, length-1-i); j++){
-                if(s.charAt(i-j) != s.charAt(i+j)){
+            for(int j=i; j <= i+1; j++){
+                if( j >= length){
                     break;
                 }
-            }
-            if(maxSubStr < 2*(j-1)+1){
-                maxSubStr = 2*(j-1)+1;
-                center = i;
-            }
-        }
-        for(int i=0; i<length-1; i++){
-            if(s.charAt(i) != s.charAt(i+1)){
-                continue;
-            }
-            int j=1;
-            for(j=1; j<=Math.min(i, length-2-i); j++){
-                if(s.charAt(i-j) != s.charAt(i+1+j)){
-                    break;
+                int temp = maxPalindorm(i,j,s);
+                if(2 * temp + j-i+1 > maxRes){
+                    maxRes = 2 * temp + j-i+1;
+                    left = i - temp;
+                    right = j + temp;
                 }
             }
-            if(maxSubStr < 2*j){
-                maxSubStr = 2*j;
-                center = i;
+        }
+        return s.substring(left, right + 1);
+    }
+
+    private int maxPalindorm(Integer c1, Integer c2, String s){
+        int step = 0;
+        while(c1-step >= 0 && c2+step < s.length()){
+            if(s.charAt(c1-step) == s.charAt(c2+step)){
+                step = step + 1;
+            }else{
+                break;
             }
         }
-        if(maxSubStr%2 != 0){
-            return s.substring(center-maxSubStr/2, center+maxSubStr/2+1);
-        }else{
-            return s.substring(center-maxSubStr/2+1, center+maxSubStr/2+1);
-        }
+        return step-1;
     }
 }
